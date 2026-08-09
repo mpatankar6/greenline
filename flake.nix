c50f4946541f1c3ddc5f49ae138e1f41bf3ffb9f
Mihir Patankar <mpatankar06@gmail.com>
2026-08-09 02:45:45 +0000

    add ci workflows (#1)

--- flake.nix
+++ flake.nix
@@ -10,29 +10,36 @@
         config.allowUnfree = true;
       };
       llvm = pkgs.llvmPackages_22;
+
+      mkPackage =
+        stdenv:
+        stdenv.mkDerivation {
+          pname = "greenline";
+          version = pkgs.lib.trim (builtins.readFile ./VERSION);
+
+          src = ./.;
+
+          nativeBuildInputs = [
+            pkgs.addDriverRunpath
+            pkgs.cmake
+            pkgs.ninja
+            pkgs.pkg-config
+          ];
+
+          buildInputs = [
+            (pkgs.linuxPackages.nvidia_x11.override { libsOnly = true; })
+            pkgs.cudaPackages.cuda_nvml_dev
+            pkgs.ncurses
+          ];
+
+          # Rewrite RPATH so libnvidia-ml.so resolves to the driver's library at runtime.
+          postFixup = "addDriverRunpath $out/bin/greenline";
+        };
     in
     {
-      packages.${system}.default = llvm.stdenv.mkDerivation {
-        pname = "greenline";
-        version = pkgs.lib.trim (builtins.readFile ./VERSION);
-
-        src = ./.;
-
-        nativeBuildInputs = [
-          pkgs.addDriverRunpath
-          pkgs.cmake
-          pkgs.ninja
-          pkgs.pkg-config
-        ];
-
-        buildInputs = [
-          (pkgs.linuxPackages.nvidia_x11.override { libsOnly = true; })
-          pkgs.cudaPackages.cuda_nvml_dev
-          pkgs.ncurses
-        ];
-
-        # Rewrite RPATH so libnvidia-ml.so resolves to the driver's library at runtime.
-        postFixup = "addDriverRunpath $out/bin/greenline";
+      packages.${system} = {
+        default = mkPackage llvm.stdenv;
+        gccCheck = mkPackage pkgs.gcc16Stdenv;
       };
 
       devShells.${system}.default = pkgs.mkShell.override { stdenv = llvm.stdenv; } {
